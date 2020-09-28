eb62a1738cdae230cbf03205cae2c4ab08d3caf5
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-09-28 17:28:41 -0300

    Adiciona listar-livros e pagina-detalhe

--- casadocodigo/src/main/java/br/com/casadocodigo/controllers/BookController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/controllers/BookController.java
@@ -3,11 +3,10 @@ import br.com.casadocodigo.models.Book;
 import br.com.casadocodigo.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -16,6 +15,7 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    @PostMapping
     public ResponseEntity<Book> createBook(@RequestBody @Valid Book book){
         if(book != null) {
             bookRepository.save(book);
@@ -24,4 +24,22 @@ public class BookController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> list(){
+
+        List<Book> books = bookRepository.findAll();
+        return ResponseEntity.ok(books);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getById(@PathVariable Long id){
+
+        Book book = bookRepository.findById(id).orElseThrow();
+
+        return ResponseEntity.ok(book);
+
+    }
+
 }
