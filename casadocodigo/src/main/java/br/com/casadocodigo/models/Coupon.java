e71381a5ba41bb925d2e61f11e49a7a65aff11e0
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-09-28 17:47:47 -0300

    Continua fluxo de pagamento

--- casadocodigo/src/main/java/br/com/casadocodigo/models/Coupon.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/models/Coupon.java
@@ -4,16 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import java.util.List;
 
 @Entity
-public class ShoppingCart {
+public class Coupon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    private List<Book> items;
 }
